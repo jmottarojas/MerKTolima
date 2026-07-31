d2258413773aa15018610280bd451c151494c294
jmottarojas <80008422+jmottarojas@users.noreply.github.com>
2026-07-31 14:05:31 -0500

    Update ALLOWED_HOSTS for production settings

--- frontend/merkatolima_frontend/settings.py
+++ frontend/merkatolima_frontend/settings.py
@@ -15,7 +15,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-merkatolima-dev-key-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = [
+    'merktolima.com',
+    'www.merktolima.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 INSTALLED_APPS = [
