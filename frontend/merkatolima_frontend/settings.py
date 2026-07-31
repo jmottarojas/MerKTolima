3bd641b7e185313c6293709c21f1aa006a2d74a9
jmottarojas <80008422+jmottarojas@users.noreply.github.com>
2026-07-31 14:31:56 -0500

    Add allowed host for Render deployment

--- frontend/merkatolima_frontend/settings.py
+++ frontend/merkatolima_frontend/settings.py
@@ -18,6 +18,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = [
     'merktolima.com',
     'www.merktolima.com',
+    'merktolima-1.onrender.com',
     'localhost',
     '127.0.0.1'
 ]
